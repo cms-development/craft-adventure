5854c91ce9f0e7d900db7654591acaa018b2a2a7
Frederick Roegiers <frederick@rogerthat.be>
2024-12-11 14:12:27 +0100

    wip

---
+++ modules/winkelmandje/src/WinkelmandjeModule.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace modules\winkelmandje;
+
+use yii\base\BootstrapInterface;
+use yii\base\Module;
+
+class WinkelmandjeModule extends Module implements BootstrapInterface
+{
+    public function init()
+    {
+        parent::init();
+        
+    }
+
+    public function bootstrap($app)
+    {
+        
+    }
+}
